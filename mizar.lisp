f1559050636ee9b83dc281dbe3b3284c55d68ea9
Jesse Alama <jesse.alama@gmail.com>
2010-11-24 19:08:45 +0000

    Define the *mizfiles* variable.

--- mizar.lisp
+++ mizar.lisp
@@ -1,4 +1,13 @@
 
 (in-package :mizar)
 
+(defparameter *mizfiles* 
+  (sb-ext:posix-getenv "MIZFILES")
+  "The directory that contains the MML and the mml.lar file.
+
+The default value is the value of the MIZFILES environment
+variable (at load time).")
+
+(defun run-mizar-utility (utility)
+
 ;;; mizar.lisp ends here
